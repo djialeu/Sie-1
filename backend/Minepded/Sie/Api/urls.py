b149fe31165b51f7d0adf50fe4ec0eb454763bae
djialeu <djialeuduprey@gmail.com>
2020-12-21 12:50:16 +0100

    ajout de l'api rest pour le sous module de gestion projets

--- backend/Minepded/Sie/Api/urls.py
+++ backend/Minepded/Sie/Api/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
 
-from .views import ThematiqueListView
+from .views import *
 
 urlpatterns = [
-    path('', ThematiqueListView.as_view())
+    path('thematiques', ThematiqueListView.as_view()),
+    path('thematiques/<pk>', ThematiqueDetailView.as_view()),
+    path('projets', ProjetListView.as_view()),
+    path('projets/<pk>', ProjetDetailView.as_view()),
+    path('programmes', ProgrammeListView.as_view()),
+    path('programmes/<pk>', ProgrammeDetailView.as_view()),
+    path('categories_mesures', CategorieMesureListView.as_view()),
+    path('categories_mesures/<pk>', CategorieMesureDetailView.as_view()),
+    path('mesures', MesureListView.as_view()),
+    path('mesures/<pk>', MesureDetailView.as_view()),
+    path('indicateurs', IndicateurListView.as_view()),
+    path('indicateurs/<pk>', IndicateurDetailView.as_view()),
 ]
